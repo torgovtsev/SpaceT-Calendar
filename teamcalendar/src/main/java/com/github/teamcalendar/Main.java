054b95d1c7cde8706114672438a93c47844357af
Sergey <tsscsf@gmail.com>
2015-10-04 12:32:42 +0400

    Init JSF project. Added "Hello world" page

--- teamcalendar/src/main/java/com/github/teamcalendar/Main.java
+++ teamcalendar/src/main/java/com/github/teamcalendar/Main.java
@@ -1,5 +1,26 @@
 package com.github.teamcalendar;
 
-public class Main {
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 
+@ManagedBean(name = "main")
+@RequestScoped
+public class Main {
+	
+	private String msg;
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+	@PostConstruct
+	private void init()
+	{
+		msg = "Hello World! I'm Teamcalendar";
+	}
 }
