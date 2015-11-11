a31312f1bc4b2948cd6823915f05ac88ff2d7e84
Ivan Kopylov <stealthwar@gmail.com>
2015-11-11 18:01:52 +0300

    #15 Login page implementation

---
+++ teamcalendar/src/main/java/com/github/teamcalendar/frontend/component/UserLoginBean.java
@@ -0,0 +1,85 @@
+package com.github.teamcalendar.frontend.component;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+import org.primefaces.context.RequestContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.github.teamcalendar.middleware.dto.User;
+import com.github.teamcalendar.middleware.services.UsersService;
+
+@ManagedBean
+@Component(value = "userLoginBean")
+@RequestScoped
+public class UserLoginBean
+{
+    @Autowired
+    private UsersService userService;
+
+    private String       username;
+    private String       password;
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public void login(ActionEvent event)
+    {
+
+        RequestContext context = RequestContext.getCurrentInstance();
+        FacesMessage message = null;
+        boolean loggedIn = false;
+
+        if (username != null && password != null)
+        {
+            if (username.contains("@") && username.contains("."))
+            {
+                User u = userService.getUserByEmail(username);
+                if (u != null)
+                {
+                    if (u.getPassword().equals(password))
+                    {
+                        loggedIn = true;
+                        message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", username);
+                        FacesContext.getCurrentInstance().addMessage(null, message);
+                        context.addCallbackParam("loggedIn", loggedIn);
+                    }
+                    else
+                    {
+                        loggedIn = false;
+                        message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Loggin Error", "Incorrect email or password");
+                        FacesContext.getCurrentInstance().addMessage(null, message);
+                    }
+                }
+                else
+                {
+                    loggedIn = false;
+                    message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Loggin Error", "No such user");
+                    FacesContext.getCurrentInstance().addMessage(null, message);
+                }
+            }
+        }
+
+    }
+}
